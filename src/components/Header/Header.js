6c6732a76445e689677b02b06b97d4415a99a8de
DaisukeFFF <ifc.dai.faculty@gmail.com>
2018-06-25 23:55:21 +0900

    change class to stateless function component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,23 @@
 import React from 'react'
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-
-        return (
-            <div>
-                <div style={{marginTop:"20px"}}>
-                    <div className="container">
-                        <div className="col-xs-12 col-sm-2 col-md-2">
-                        </div>
-                        <div className="col-xs-12 col-sm-8 col-md-8">
-                            <center>
-                                <a href="/main">
-                                    <img className="logo" src="image/assets/logo_white_02.png" alt="title" />
-                                </a>
-                            </center>
-                        </div>
-                        <div className="col-xs-12 col-sm-2 col-md-2">
-                        </div>
-                    </div>
+const Header = () => (
+    <div>
+        <div style={{marginTop: "20px"}}>
+            <div className="container">
+                <div className="col-xs-12 col-sm-2 col-md-2">
+                </div>
+                <div className="col-xs-12 col-sm-8 col-md-8">
+                    <center>
+                        <a href="/main">
+                            <img className="logo" src="image/assets/logo_white_02.png" alt="title"/>
+                        </a>
+                    </center>
+                </div>
+                <div className="col-xs-12 col-sm-2 col-md-2">
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    </div>
+);
 
 export default Header
